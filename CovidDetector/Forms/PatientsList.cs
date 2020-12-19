bfc77c65d972d28ec1aedea39e544d85e84b690a
Jofre <jofre@google.com>
2020-12-19 18:21:33 +0100

    Forms creados

---
+++ CovidDetector/Forms/PatientsList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CovidDetector.Forms
+{
+    public partial class PatientsList : Form
+    {
+        public PatientsList()
+        {
+            InitializeComponent();
+        }
+    }
+}
