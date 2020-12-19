3636ee9a25c18d783c598ba9454f1360f5138408
elyRP <ely7787@gmail.com>
2020-12-19 20:59:32 +0100

    init pattien form

--- CovidDetector/Forms/NewPatientForm.cs
+++ CovidDetector/Forms/NewPatientForm.cs
@@ -16,5 +16,37 @@ namespace CovidDetector.Forms
         {
             InitializeComponent();
         }
+
+                public void setColors()
+                {
+                    Color buttonColor = ColorTranslator.FromHtml("#56BEE7");
+                    Color buttonOverColor = ColorTranslator.FromHtml("#349CC5");
+                    this.button1.BackColor = buttonColor;
+                    this.button1.ForeColor = Color.White;
+                    this.button2.BackColor = buttonColor;
+                    this.button2.ForeColor = Color.White;
+                    button1.FlatAppearance.MouseOverBackColor = buttonOverColor;
+                    button2.FlatAppearance.MouseOverBackColor = buttonOverColor;
+
+
+                }
+
+                private void button1_Click(object sender, EventArgs e)
+                {
+                    LoginModule login = new LoginModule(this.patientInput.Text, this.cardiopathy___yesInput.Text);
+                    if (login.validate())
+                    {
+                        var medicPanelForm = new MedicUXForm();
+                        this.Hide();
+                        medicPanelForm.ShowDialog();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Check the password and try again", "Invalid Password",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
     }
 }
---
+++ CovidDetector/Modules/NewPatientModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidDetector.Modules
+{
+    public class NewPatientModule
+    {
+        public string patient { get; set; }
+        public string cardiopathy___yes { get; set; }
+        public string cardiopathy___no { get; set; }
+        public string cardiopathy___null { get; set; }
+
+        public string hypertension___yes { get; set; }
+        public string hypertension___no { get; set; }
+        public string hypertension___null { get; set; }
+
+        public string hypertension___yes { get; set; }
+        public string hypertension___no { get; set; }
+        public string hypertension___null { get; set; }
+
+        public string hypertension___yes { get; set; }
+        public string hypertension___no { get; set; }
+        public string hypertension___null { get; set; }
+
+        public NewPatientModule(string patient, string cardiopathy___yes, string cardiopathy___no, string cardiopathy___null)
+        {
+            this.patient = patient;
+            this.cardiopathy___yes = cardiopathy___yes;
+            this.cardiopathy___no = cardiopathy___no;
+            this.cardiopathy___null = cardiopathy___null;
+
+
+        }
+        public bool validate()
+        {
+            return this.user == "medic1" && this.password == "12345";
+        }
+    }
+}
